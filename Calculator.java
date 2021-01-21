8ba3d0ec50fefa2f07cb4cdac91f8bfaca3728cb
Thilina Chathuranga <pdtcrc@gmail.com>
2021-01-21 21:27:30 +0530

    Update calculator with company name

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,6 @@ public class Calculator {
 
   public static main(String[] args) {
   
-    System.out.println("This is the calculator software of the company.");
+    System.out.println("This is the calculator software of the company called ABC.");
   }
 }
